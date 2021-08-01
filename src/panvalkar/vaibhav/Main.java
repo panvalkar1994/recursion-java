647d29c6a0f56e93ecee9a6f03a19e97c87a4c2f
vaibhav panvalkar <vpanvalkar@outlook.com>
2021-08-01 21:00:34 +0530

    printing array

--- src/panvalkar/vaibhav/Main.java
+++ src/panvalkar/vaibhav/Main.java
@@ -1,5 +1,6 @@
 package panvalkar.vaibhav;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -7,5 +8,15 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter size of array");
+        int n = scanner.nextInt();
+        System.out.println("Enter Array values: ");
+        int[] arr = new int[n];
+        for (int i=0; i<n; i++){
+            int x = scanner.nextInt();
+            arr[i] = x;
+        }
+        System.out.println("Printing array");
+        System.out.println(Arrays.toString(arr));
     }
 }
