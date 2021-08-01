45005cf400be1cffb644e755b66c532a7d9394aa
vaibhav panvalkar <vpanvalkar@outlook.com>
2021-08-01 19:45:40 +0530

    Scanner added

--- src/panvalkar/vaibhav/Main.java
+++ src/panvalkar/vaibhav/Main.java
@@ -1,9 +1,11 @@
 package panvalkar.vaibhav;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello world");
+        Scanner scanner = new Scanner(System.in);
     }
 }
