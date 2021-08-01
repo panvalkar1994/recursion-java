fdbfa0b12eb449e275eef9ec8a0db2db91323e97
vaibhav panvalkar <vpanvalkar@outlook.com>
2021-08-01 21:38:46 +0530

    first occurrence of a value

--- src/panvalkar/vaibhav/Main.java
+++ src/panvalkar/vaibhav/Main.java
@@ -7,5 +7,24 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter size of array");
+        int n = scanner.nextInt();
+        System.out.println("Enter Target value");
+        int target = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i=0; i<n; i++){
+            int x = scanner.nextInt();
+            arr[i]=x;
+        }
+        int first = firstOccurrence(arr, 0, target);
+        System.out.println("first occurrence of "+target+" is at "+first);
+    }
+
+    public static int firstOccurrence(int[] arr, int idx, int target){
+        if (idx == arr.length -1){
+            return -1;
+        }
+        if (arr[idx]==target) return idx;
+        return firstOccurrence(arr, idx+1, target);
     }
 }
