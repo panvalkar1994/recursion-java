df38d6be964c9d59b4a6434fa771b371266746e4
vaibhav panvalkar <vpanvalkar@outlook.com>
2021-08-01 20:36:54 +0530

    Tower of Hanoi solved using recursion

--- src/panvalkar/vaibhav/Main.java
+++ src/panvalkar/vaibhav/Main.java
@@ -7,5 +7,15 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Scanner scanner = new Scanner(System.in);
+        toh(3, "A", "B", "C");
+    }
+
+    public static void toh(int n, String start, String end, String helper){
+        if (n==0){
+            return;
+        }
+        toh(n-1, start, helper, end);
+        System.out.println(n+""+start+"->"+end);
+        toh(n-1,helper, end, start);
     }
 }
