a0ee3ded2242b7ad6a8581918238559da12486a3
vaibhav panvalkar <vpanvalkar@outlook.com>
2021-08-01 17:53:26 +0530

    factorial function added

--- src/panvalkar/vaibhav/Main.java
+++ src/panvalkar/vaibhav/Main.java
@@ -1,9 +1,22 @@
 package panvalkar.vaibhav;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello world");
+        Scanner scn  =  new Scanner(System.in);
+        System.out.println("Enter number to find factorial");
+        int n  = scn.nextInt();
+        int fact = factorial(n);
+        System.out.println("factorial of "+n+" is "+fact);
+    }
+
+    public  static  int factorial(int n){
+        if(n==0){
+            return 1;
+        }
+        return n * factorial(n-1);
     }
 }
