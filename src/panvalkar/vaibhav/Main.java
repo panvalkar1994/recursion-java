00675a92ffffd020cd2f5109e42893ce513a6435
vaibhav panvalkar <vpanvalkar@outlook.com>
2021-08-01 21:28:06 +0530

    maximum of array

--- src/panvalkar/vaibhav/Main.java
+++ src/panvalkar/vaibhav/Main.java
@@ -7,5 +7,20 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the size of Array");
+        int size = scanner.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            int x = scanner.nextInt();
+            arr[i] = x;
+        }
+        int maxVal = maxValue(arr,0);
+        System.out.println(maxVal);
+    }
+
+    public static int maxValue(int[] arr, int start){
+        if (start==arr.length-1) return arr[start];
+        int mxRest = maxValue(arr, start+1);
+        return Math.max(mxRest,arr[0]);
     }
 }
