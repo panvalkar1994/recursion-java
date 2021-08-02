9481ee2d8826c4675fc2fb9a793eee53e6b8dc35
vaibhav panvalkar <vpanvalkar@outlook.com>
2021-08-02 21:48:24 +0530

    last occurrence of a value

--- src/panvalkar/vaibhav/Main.java
+++ src/panvalkar/vaibhav/Main.java
@@ -1,9 +1,37 @@
 package panvalkar.vaibhav;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello world");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter size of the array");
+        int n = scanner.nextInt();
+        System.out.println("Enter Target value");
+        int d = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            int x = scanner.nextInt();
+            arr[i] = x;
+        }
+        int lastOccurrence = findLastOccurrence(arr,0,d);
+        System.out.println("Last occurrence in arr is at index: "+lastOccurrence);
+    }
+
+    public static int findLastOccurrence(int[] arr, int start, int val){
+        if (arr.length == start){
+            return -1;
+        }
+        int lastIdx = findLastOccurrence(arr, start+1, val);
+        if (lastIdx==-1){
+            if (arr[start]==val){
+                return start;
+            }else{
+                return -1;
+            }
+        }else{
+            return lastIdx;
+        }
     }
 }
